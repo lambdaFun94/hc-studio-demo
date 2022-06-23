d490bd06c90f5cded5dbcd83a8c3813dbd04656f
Max Shaw <Maxdshaw@gmail.com>
2022-06-23 17:42:17 -0600

    Update PageLayout.tsx

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -13,7 +13,7 @@ const PageLayout = ({ title, children, subtitle, image }: Props) => {
   return (
     <div className="py-12 max-w-screen-lg px-4 mx-auto">
       <div className="flex gap-4">
-        {image && <Img image={image} className="max-w-sm" />}
+        {image && <Img image={image} width={300} layout="fixed" />}
         <div>
           <div className="text-4xl font-medium mb-4">{title}</div>
           {subtitle && <div className="text-gray-700 mb-4">{subtitle}</div>}
